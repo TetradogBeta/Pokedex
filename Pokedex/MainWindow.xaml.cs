3f43977b665b996b955e71a07a5182a2cfe314c8
tetraadog <tetradog@gmail.com>
2016-05-20 10:59:12 +0200

    commit

--- Pokedex/MainWindow.xaml.cs
+++ Pokedex/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace Pokedex
         bool hayCambios;
         public MainWindow()
         {
+         
             hayCambios = false;
             InitializeComponent();
             pltNormal.ColorPicker.IsAlfaEnabled = false;
