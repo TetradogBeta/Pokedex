41754205c5a29a37b697f02f618dc33d810ab327
tetraadog <tetradog@gmail.com>
2016-09-08 22:30:01 +0200

    commit

--- Pokedex/MainWindow.xaml.cs
+++ Pokedex/MainWindow.xaml.cs
@@ -193,15 +193,14 @@ namespace Pokedex
                         cmbObjeto1.ItemsSource = romData.Objetos;
                         cmbTipo1.ItemsSource = romData.Tipos;
                         cmbTipo2.ItemsSource = romData.Tipos;
-                      /*  PokeballBattle[] pokeballs = PokeballBattle.GetPokeballsBattle(rom, romData.Edicion, romData.Compilacion);
-                        System.Windows.Controls.Image imgPokeball;
+                        System.Windows.Controls.Image imgItem;
 
-                        for (int i = 0; i < pokeballs.Length; i++)
+                        for (int i = 0; i < romData.Objetos.Count; i++)
                         {
-                            imgPokeball = new System.Windows.Controls.Image();
-                            imgPokeball.SetImage(pokeballs[i].Icono[0]);//hacer conversion de bloqueImagen a Bitmap devolviendo img[0]
-                            ugPokedex.Children.Add(imgPokeball);
-                        }*/
+                            imgItem = new System.Windows.Controls.Image();
+                            imgItem.SetImage(romData.Objetos[i].ImagenObjeto[0]);//hacer conversion de bloqueImagen a Bitmap devolviendo img[0]
+                            ugPokedex.Children.Add(imgItem);
+                        }
                         totalEntradas = DescripcionPokedex.TotalEntradas(rom, romData.Edicion, romData.Compilacion);
                         //missigno es un pokemon especial porque el orden nacional no tiene...y coge el de Mew...y para poderlo tener correctamente lo pongo a mano
                         pokemon = new PokemonPokedex(Pokemon.GetPokemon(rom, romData.Edicion, romData.Compilacion, 0, totalEntradas));//para coger la pokedex se usa el orden nacional no el de la gameFreak
@@ -222,7 +221,7 @@ namespace Pokedex
 
                         pokedexCargada = ugPokedex.Children.OfType<PokemonPokedex>().ToArray();
                         ugPokedex.Children.Sort();
-                        PonPokemon(ugPokedex.Children[1] as PokemonPokedex);
+                        PonPokemon(pokedexCargada[0]);
                         Title = "Pokédex -" + rom.NombreRom;
                     }
                     catch (Exception ex)
