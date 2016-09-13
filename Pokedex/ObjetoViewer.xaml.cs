d23449adff695f9d236b9c215e5f4850ac21f875
tetraadog <tetradog@gmail.com>
2016-09-13 02:53:34 +0200

    ahora se ven las imagenes de los objetos :D

---
+++ Pokedex/ObjetoViewer.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Gabriel.Cat.Extension;
+using PokemonGBAFrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pokedex
+{
+    /// <summary>
+    /// Interaction logic for ObjetoViewer.xaml
+    /// </summary>
+    public partial class ObjetoViewer : UserControl
+    {
+        Objeto obj;
+        public ObjetoViewer()
+        {
+            InitializeComponent();
+        }
+        public ObjetoViewer(Objeto obj) : this()
+        {
+            Objeto = obj;
+        }
+
+        public Objeto Objeto
+        {
+            get
+            {
+                return obj;
+            }
+
+            set
+            {
+                obj = value;
+                txtNombreObjeto.Text = obj.Nombre;
+                if(obj.ImagenObjeto!=null)
+                   imgObjeto.SetImage(obj.ImagenObjeto);
+            }
+        }
+    }
+}
